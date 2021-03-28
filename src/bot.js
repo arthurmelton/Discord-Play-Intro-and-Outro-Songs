923e0e9e9855afda2179e73e85b30ca84e40a043
Arthur Melton <amtitan@icloud.com>
2021-03-28 17:29:39 -0400

    cleaned the code

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,19 @@
-
 require("dotenv").config();
 const ytdl = require('ytdl-core');
 
-const { Client } = require('discord.js');
+const {
+	Client
+} = require('discord.js');
 const client = new Client();
 const PREFIX = "!S";
 
 client.on('ready', () => {
 	console.log(`${client.user.tag} bot is on`);
-	client.user.setActivity(`${PREFIX}help`, { type: 'WATCHING' })
-  		.then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
-  		.catch(console.error);
+	client.user.setActivity(`${PREFIX}help`, {
+			type: 'WATCHING'
+		})
+		.then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+		.catch(console.error);
 })
 
 client.on('message', (message) => {
@@ -22,43 +25,49 @@ client.on('message', (message) => {
 		.substring(PREFIX.length)
 		.split(/\s+/);
 	if (commandName === "join") {
-		const { voice } = message.member;
+		const {
+			voice
+		} = message.member;
 		if (!voice.channelID) {
 			message.reply("You have to be in a voice channel");
 			return;
 		}
 
-		const streamOptions = { seek: 0, volume: 1 };
+		const streamOptions = {
+			seek: 0,
+			volume: 1
+		};
 		voice.channel.join().then(connection => {
 
-            //const stream = ytdl(`https://www.youtube.com/watch?v=MFw3E6X5aoA`, { filter : 'audioonly' });
-            const stream = 'joined.mp3';
-            const dispatcher = connection.play(stream, streamOptions);
-            client.on('voiceStateUpdate', (oldMember, newMember) => {
-			  	console.log(oldMember);
-			  	if (oldMember.serverDeaf !== null) {
-			  		if (oldMember.serverDeaf === newMember.serverDeaf && oldMember.serverMute === newMember.serverMute && oldMember.selfDeaf == newMember.selfDeaf && oldMember.selfMute === newMember.selfMute && oldMember.selfVideo === newMember.selfVideo && oldMember.streaming === newMember.streaming) {}else {return} 
-			  	}
-			  	//console.log(newMember);
-			    if (oldMember.channelID === null && newMember.channelID !== null) {
-			      	console.log("User joined!");
-			      	//const stream = ytdl(`https://www.youtube.com/watch?v=MFw3E6X5aoA`, { filter : 'audioonly' });
-            		const stream = 'joined.mp3';
-            		const dispatcher = connection.play(stream, streamOptions);
-			    } else {
-			      	console.log("User left!");
-			      	//const stream = ytdl(`https://www.youtube.com/watch?v=mN7ai6ql8bQ`, { filter : 'audioonly' });
-            		const stream = 'left.mp3';
-            		const dispatcher = connection.play(stream, streamOptions);
-			    }
-			    if (oldMember.guild.channels.cache.get(oldMember.channelID)) {
-				    if (oldMember.guild.channels.cache.get(oldMember.channelID).members.size === 1) {
-				    	oldMember.guild.channels.cache.get(oldMember.channelID).leave();
-				    	return;
-				    }
+			//const stream = ytdl(`https://www.youtube.com/watch?v=MFw3E6X5aoA`, { filter : 'audioonly' });
+			const stream = 'joined.mp3';
+			const dispatcher = connection.play(stream, streamOptions);
+			client.on('voiceStateUpdate', (oldMember, newMember) => {
+				if (oldMember.serverDeaf !== null) {
+					if (oldMember.serverDeaf === newMember.serverDeaf && oldMember.serverMute === newMember.serverMute && oldMember.selfDeaf == newMember.selfDeaf && oldMember.selfMute === newMember.selfMute && oldMember.selfVideo === newMember.selfVideo && oldMember.streaming === newMember.streaming) {} else {
+						return
+					}
+				}
+				//console.log(newMember);
+				if (oldMember.channelID === null && newMember.channelID !== null) {
+					console.log("User joined!");
+					//const stream = ytdl(`https://www.youtube.com/watch?v=MFw3E6X5aoA`, { filter : 'audioonly' });
+					const stream = 'joined.mp3';
+					const dispatcher = connection.play(stream, streamOptions);
+				} else {
+					console.log("User left!");
+					//const stream = ytdl(`https://www.youtube.com/watch?v=mN7ai6ql8bQ`, { filter : 'audioonly' });
+					const stream = 'left.mp3';
+					const dispatcher = connection.play(stream, streamOptions);
+				}
+				if (oldMember.guild.channels.cache.get(oldMember.channelID)) {
+					if (oldMember.guild.channels.cache.get(oldMember.channelID).members.size === 1) {
+						oldMember.guild.channels.cache.get(oldMember.channelID).leave();
+						return;
+					}
 				}
 			})
-        }).catch(err => console.log(err));
+		}).catch(err => console.log(err));
 	}
 
 })
